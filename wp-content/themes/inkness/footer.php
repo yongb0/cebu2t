4b1b69b0b11cb60fcad64aabbc1a7d4ef730f761
Lester Padul <padullester@gmail.com>
2015-06-01 05:54:35 +0000

    fixed fancybox

--- wp-content/themes/inkness/footer.php
+++ wp-content/themes/inkness/footer.php
@@ -20,7 +20,8 @@
 // external js: isotope.pkgd.js
 
 jQuery(document).ready( function() {
-  
+    
+  // set timeout before executing masonry
   setTimeout(function(){  
     jQuery('.grid').isotope({
       itemSelector: 'article',
@@ -28,7 +29,7 @@ jQuery(document).ready( function() {
         columnWidth: 2
       }
     });
-  },500);
+  },300);
   
   
   /*jQuery('.fdc p').readmore({
@@ -57,10 +58,11 @@ jQuery(document).ready( function() {
         });
       }
   });*/
-  
-
+    
+  //count the number of archives in the post
   var countChecker = jQuery('#main').find('article.archive').length;
- 
+  
+  //check if counter is greater than 0
   if(countChecker>0){
     jQuery('#main').find('article.archive').each(function(i,e){
       var content = jQuery(e).find('.entry-content').html();
@@ -69,17 +71,53 @@ jQuery(document).ready( function() {
       var contentLink  = jQuery(e).find('h1.entry-title a').attr("href");
 
       if(contentCheck.length>200){
-        contentCheck   = contentCheck.substr(0,200);
-        contentCheck += "<div style='padding:10px; padding-left:0px;'><a href='"+contentLink+"'>Read More</a></div>";
+        contentCheck   = contentCheck.substr(0,200)+"...";
+        contentCheck += "<div style='padding:10px; padding-left:0px;'><a href='"+contentLink+"'>Continue Reading...</a></div>";
       }
 
       jQuery(e).find('.entry-content').html(contentCheck);
       
     });
+  } 
+
+  //get the page's class
+  var checkIfSingle = jQuery('#main').find('article.singular-item');
+
+  var fancyBox = false;
+
+  //check if the page is a single content
+  if(checkIfSingle.length!==0) {
+    var images = checkIfSingle.find(".entry-content img");//get all the images inside the content area
+
+    //check if the images is not 0
+    if(images.length!==0){
+      checkIfSingle.find('.entry-content img').each(function(i,e){
+        //get the non-emoji images
+        if(!jQuery(e).hasClass('emoji')){
+          var imageParent = jQuery(e).parent();
+          var parentTagName = jQuery.trim(imageParent.prop("tagName").toLowerCase());
+          if(parentTagName!="a"){
+            var imageSrc = jQuery(e).attr("src"); //get the image src
+            jQuery(e).wrap("<a href='"+imageSrc+"' class='fancybox'></a>"); //wrap image around anchor tag
+            fancyBox = true; //set the fancybox
+          }
+        }
+      });
+    }
   }
   
+  //check if fancybox was applied
+  if(fancyBox) {
+   jQuery("a.fancybox").fancybox();//reinitialize fancybox
+  }
+
 });
 
+/**
+ * [stripHTML description]
+ * @param  {[type]} dirtyString [description]
+ * @return {[type]}             [description]
+ */
 function stripHTML(dirtyString) {
     var container = document.createElement('div');
     container.innerHTML = dirtyString;
